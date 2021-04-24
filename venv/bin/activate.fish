058410994b5ae67b051d46754fe42c9cf6a55761
Jiya Ull Haq <60106386+Jiya-Ull-Haq@users.noreply.github.com>
2021-04-24 14:01:58 +0530

    NewVersion_FlaskCode [V1.5]

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jiyaullhaq/Downloads/Hustle/Hustle/venv"
+set -gx VIRTUAL_ENV "/Users/jiyaullhaq/PycharmProjects/flaskProject/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
